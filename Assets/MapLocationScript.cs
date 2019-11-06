404f9622adb3ea891353955861f84ec38c8af9bc
Arjun Nemani <nemaniarjun@gmail.com>
2019-11-07 00:53:04 +0530

    init

---
+++ Assets/MapLocationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLocationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		self.PlayerNumber = 0;
+        Debug.log("init!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.log("update!");
+	}
+	
+	public int PlayerNumber;
+	public GameObject PlaceHolder;
+
+}
